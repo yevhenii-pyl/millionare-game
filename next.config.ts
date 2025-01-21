7f28742825b54f59de494c2a9463ac629d877ff6
Yevhenii Pylypchuk <flyhighthendie@gmail.com>
2025-01-21 12:00:25 +0200

    chore(setup): init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  ignoreDuringBuilds: true,
 };
 
 export default nextConfig;
